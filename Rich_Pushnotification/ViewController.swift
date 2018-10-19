83559c43e8014d17d2b5ffe8bee4a7e306cc4c59
Neil Patel <neil@neils-mac-mini.local>
2018-10-19 18:58:45 +0530

    Initial Commit

---
+++ Rich_Pushnotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rich_Pushnotification
+//
+//  Created by Neil Patel on 10/19/18.
+//  Copyright © 2018 Pankaj Gondaliya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
